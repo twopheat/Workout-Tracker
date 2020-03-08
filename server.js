8c08348698336adb8f24092fe787d788eff988fd
twopheat <twopheat@gmail.com>
2020-03-07 23:22:20 -0800

    heroku fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const databaseUrl = "workoutDB";
+const databaseUrl = "heroku_mcxg3rd9";
 const collections = ["workouts"];
 
 const db = mongojs(databaseUrl, collections);
